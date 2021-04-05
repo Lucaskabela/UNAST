6138dc003d15e89bfcbe4cca0050e19f4453ca04
Bill Yang <bill@Bills-MBP.attlocal.net>
2021-04-05 15:45:58 -0500

    added script to download and preprocess dataset

---
+++ src/get_dataset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+# Create the data folder
+mkdir -p data
+cd data
+
+# Download the LJSpeech dataset into the data folder
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+tar -xvf LJSpeech-1.1.tar.bz2
+
+cd ../
+python prepare_data.py
