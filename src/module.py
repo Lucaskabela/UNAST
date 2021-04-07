5daee0c7f29521811959d01b4e9d503d04ec0fe8
Jie Hao Liao <liaojh1998@gmail.com>
2021-04-07 13:45:25 -0500

    Added text prenet and its testing notebook

--- src/module.py
+++ src/module.py
@@ -5,6 +5,64 @@ processing modules for text and speech.
 '''
 # TODO: Consider adding pre/post net base class.
 
+from data.symbols import symbols
+
+class Linear(nn.Module):
+    """
+    Linear Module copied from Transformer-TTS.
+    
+    A linear layer initialized using the Xavier Uniform method.
+    """
+    def __init__(self, in_dim, out_dim, bias=True, w_init='linear'):
+        """
+        :param in_dim: dimension of input
+        :param out_dim: dimension of output
+        :param bias: boolean. if True, bias is included.
+        :param w_init: str. weight inits with xavier initialization.
+        """
+        super(Linear, self).__init__()
+        self.linear_layer = nn.Linear(in_dim, out_dim, bias=bias)
+
+        nn.init.xavier_uniform_(
+            self.linear_layer.weight,
+            gain=nn.init.calculate_gain(w_init))
+
+    def forward(self, x):
+        return self.linear_layer(x)
+
+class Conv(nn.Module):
+    """
+    Convolution Module copied from Transformer-TTS.
+    
+    A 1D convolution layer initialized using the Xavier Uniform
+    method.
+    """
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1,
+                 padding=0, dilation=1, bias=True, w_init='linear'):
+        """
+        :param in_channels: dimension of input
+        :param out_channels: dimension of output
+        :param kernel_size: size of kernel
+        :param stride: size of stride
+        :param padding: size of padding
+        :param dilation: dilation rate
+        :param bias: boolean. if True, bias is included.
+        :param w_init: str. weight inits with xavier initialization.
+        """
+        super(Conv, self).__init__()
+
+        self.conv = nn.Conv1d(in_channels, out_channels,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, dilation=dilation,
+                              bias=bias)
+
+        nn.init.xavier_uniform_(
+            self.conv.weight, gain=nn.init.calculate_gain(w_init))
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
 class SpeechPrenet(nn.Module):
     # TODO: Fill in from TTS repo :) 
     def __init__(self):
@@ -16,9 +74,58 @@ class SpeechPostnet(nn.Module):
         super(SpeechPostnet, self).__init__()
 
 class TextPrenet(nn.Module):
-    # TODO: Fill in from TTS repo :) 
-    def __init__(self):
+    """
+    Pre-network for Text Encoder copied from Transformer-TTS.
+    
+    Essentially, maps phoneme IDs to an embedding space using
+    convolutional networks. Should be the same as Ren's paper
+    since it outputs an embedding of size 256 for each phoneme.
+    """
+    def __init__(self, embedding_size, num_hidden):
+        """
+        :param embedding_size: phoneme embedding size
+        :param num_hidden: output embedding size
+        """
         super(TextPrenet, self).__init__()
+        self.embedding_size = embedding_size
+        self.embed = nn.Embedding(len(symbols), embedding_size, padding_idx=0)
+
+        self.conv1 = Conv(in_channels=embedding_size,
+                          out_channels=num_hidden,
+                          kernel_size=5,
+                          padding=int(np.floor(5 / 2)),
+                          w_init='relu')
+        self.conv2 = Conv(in_channels=num_hidden,
+                          out_channels=num_hidden,
+                          kernel_size=5,
+                          padding=int(np.floor(5 / 2)),
+                          w_init='relu')
+
+        self.conv3 = Conv(in_channels=num_hidden,
+                          out_channels=num_hidden,
+                          kernel_size=5,
+                          padding=int(np.floor(5 / 2)),
+                          w_init='relu')
+
+        self.batch_norm1 = nn.BatchNorm1d(num_hidden)
+        self.batch_norm2 = nn.BatchNorm1d(num_hidden)
+        self.batch_norm3 = nn.BatchNorm1d(num_hidden)
+
+        self.dropout1 = nn.Dropout(p=0.2)
+        self.dropout2 = nn.Dropout(p=0.2)
+        self.dropout3 = nn.Dropout(p=0.2)
+        self.projection = Linear(num_hidden, num_hidden)
+
+    def forward(self, input_):
+        input_ = self.embed(input_) 
+        input_ = input_.transpose(1, 2) 
+        input_ = self.dropout1(torch.relu(self.batch_norm1(self.conv1(input_)))) 
+        input_ = self.dropout2(torch.relu(self.batch_norm2(self.conv2(input_)))) 
+        input_ = self.dropout3(torch.relu(self.batch_norm3(self.conv3(input_)))) 
+        input_ = input_.transpose(1, 2) 
+        input_ = self.projection(input_) 
+
+        return input_
 
 class TextPostnet(nn.Module):
     # TODO: Fill in from TTS repo :) 
