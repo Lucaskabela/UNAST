dced15e0651a9d06b882813446683ec03e04c350
Lucaskabela <lucaskabela@utexas.edu>
2021-04-29 07:52:39 -0500

    Improvements

--- src/viz.py
+++ src/viz.py
@@ -5,30 +5,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 def twod_viz(args, model=None, num_points=5):
+
     device = init_device()
     test_dataset = get_dataset('test.csv')
     test_dataloader = DataLoader(test_dataset,
             batch_size=num_points,
             collate_fn=collate_fn_transformer,
             num_workers=1)
-    text_m, speech_m, discriminator, teacher = None, None, None, get_teacher_ratio(args)
-    if args.model_type == 'rnn':
-        text_m = TextRNN(args)
-        speech_m = SpeechRNN(args)
-    elif args.model_type == 'transformer':
-        text_m = TextTransformer(args)
-        speech_m = SpeechTransformer(args)
-
-    if args.use_discriminator:
-        discriminator_in_dim = args.hidden * 2 if args.model_type == 'rnn' else args.hidden
-        discriminator = LSTMDiscriminator(discriminator_in_dim, args.disc_hid, bidirectional=args.disc_bidirectional, num_layers=args.disc_num_layers)
-    model = UNAST(text_m, speech_m, discriminator, teacher).to(DEVICE)
-    if args.load_path is not None:
-        if os.path.isfile(args.load_path):
-            _, best, model, _ = load_ckp(args.load_path, model, optimizer)
-        else:
-            print(f"[WARN] Could not find checkpoint '{args.load_path}'.")
-            print(f"[WARN] Training from initial model...")
+    model = initialize_model(args)
     model.eval()
 
     for batch in test_dataloader:
@@ -50,13 +34,14 @@ def twod_viz(args, model=None, num_points=5):
 
    
     fig = plt.figure()
-
+    labels = ['text', 'speech']
     for idx, cl in enumerate(np.unique(labels)):
-        x = [principalComponents[idx, 0], principalComponents[idx + num_points, 0]]
-        y = [principalComponents[idx, 1], principalComponents[idx + num_points, 1]]
-        plt.scatter(x=x,
-                    y=y,
-                    label=idx)
+        plt.scatter(x=x[idx * num_points : (idx + 1) * num_points ],
+                    y=y[idx * num_points : (idx + 1) * num_points ],
+                    label=cl,)
+    i = 0
+    for x, y in zip(xs, ys):
+        plt.text(x, y, str(i % num_points), fontsize=12)
 
     plt.legend(loc='upper left')
     plt.xlim(-2.75, 2.75)
@@ -66,3 +51,14 @@ def twod_viz(args, model=None, num_points=5):
     plt.title("TITLE GOES HERE")
     plt.show()
     fig.savefig('latent_2d_viz.png')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='JSON config files')
+    args = parse_with_config(parser)
+
+    global DEVICE
+    DEVICE = init_device(args)
+
+    twod_viz(args)
